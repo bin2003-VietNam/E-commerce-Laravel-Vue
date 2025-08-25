07899b881bcc46f1ed0f674fe322c7310f5a99df
Nguyen Van Bin <bin2003mail@gmail.com>
2025-08-25 19:18:49 +0700

    seperate frontend and backend

---
+++ Frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div class="h-screen w-screen flex justify-center items-center bg-amber-200">
+      <div>
+        Tailwind worked
+      </div>
+  </div>
+</template>
+
+<style scoped></style>
