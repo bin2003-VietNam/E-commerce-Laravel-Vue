7d60b16c509c36b21e12035a817aee17da446305
Nguyen Van Bin <bin2003mail@gmail.com>
2025-08-26 17:42:48 +0700

    chore: add product section

--- Frontend/src/views/home.vue
+++ Frontend/src/views/home.vue
@@ -6,6 +6,7 @@ import userIcon from "@/assets/images/user.png"
 import banner1 from "@/assets/images/banner1.jpg"
 import banner2 from "@/assets/images/banner2.jpg"
 import banner3 from "@/assets/images/banner3.jpg"
+import iphone from "@/assets/images/iphone.jpg"
 
 import phone from "@/assets/images/phone.png"
 import watch from "@/assets/images/watch.png"
@@ -50,6 +51,7 @@ import { Autoplay, Pagination, Navigation } from 'swiper/modules';
                 <img :src="userIcon" class="w-7 h-7" />
             </div>
         </div>
+
         <!-- Banner -->
         <div class="w-screen max-h-[632px]">
             <Swiper :modules="[Autoplay, Pagination, Navigation]" :slides-per-view="1" :autoplay="{ delay: 3000 }" loop
@@ -73,29 +75,180 @@ import { Autoplay, Pagination, Navigation } from 'swiper/modules';
                 <div class="flex flex-row gap-10 justify-center items-center">
                     <div class="w-40 h-40 bg-gray-200 rounded-4xl flex flex-col justify-center items-center gap-3">
                         <img :src="phone" class="w-15 h-15" />
-                        <p class="text-xl font-bold">Phones</p>
+                        <p class="text-xl ">Phones</p>
                     </div>
                     <div class="w-40 h-40 bg-gray-200 rounded-4xl flex flex-col justify-center items-center gap-3">
                         <img :src="watch" class="w-15 h-15" />
-                        <p class="text-xl font-bold">Phones</p>
+                        <p class="text-xl ">Phones</p>
                     </div>
                     <div class="w-40 h-40 bg-gray-200 rounded-4xl flex flex-col justify-center items-center gap-3">
                         <img :src="camera" class="w-15 h-15" />
-                        <p class="text-xl font-bold">Phones</p>
+                        <p class="text-xl ">Phones</p>
                     </div>
                     <div class="w-40 h-40 bg-gray-200 rounded-4xl flex flex-col justify-center items-center gap-3">
                         <img :src="headphone" class="w-15 h-15" />
-                        <p class="text-xl font-bold">Phones</p>
+                        <p class="text-xl ">Phones</p>
                     </div>
                     <div class="w-40 h-40 bg-gray-200 rounded-4xl flex flex-col justify-center items-center gap-3">
                         <img :src="monitor" class="w-15 h-15" />
-                        <p class="text-xl font-bold">Phones</p>
+                        <p class="text-xl ">Phones</p>
                     </div>
                     <div class="w-40 h-40 bg-gray-200 rounded-4xl flex flex-col justify-center items-center gap-3">
                         <img :src="gamepad" class="w-15 h-15" />
-                        <p class="text-xl font-bold">Phones</p>
+                        <p class="text-xl ">Phones</p>
                     </div>
                 </div>
+            </div>
+        </div>
+
+        <!-- Product -->
+        <div class="w-screen h-[1056px] bg-white px-30 py-10 text-black items-center overflow-auto">
+            <div class="w-full h-full">
+                <div class="flex flex-row items-start gap-4 mb-10">
+                    <div class="w-auto inline-block  border-b-3 border-black font-bold text-xl">New Arrival</div>
+                    <div class="w-auto inline-block  border-b-3 border-black font-bold text-xl">Best Seller</div>
+                    <div class="w-auto inline-block  border-b-3 border-black font-bold text-xl">Featured Product</div>
+                </div>
+
+                <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4 place-items-center ">
+                    <div class="max-w-60 h-100 bg-gray-200 rounded-xl flex flex-col gap-3 shadow-xl">
+                        <div class="w-full flex justify-end mb-2 pr-6 pt-3">
+                            <div class="w-7 h-7">
+                                <img :src="heartIcon"/>
+                            </div>
+                        </div>
+                        <div class="w-full h-40 mb-1">
+                            <img :src="iphone" class="w-full h-full object-cover"/>
+                        </div>
+                        <div class="flex flex-col justify-center items-center ">
+                            <p class="text-center">Apple Iphone 14 Pro Max 128GB Deep Pink</p>
+                            <p class="font-bold text-3xl">$900</p>
+                        </div>
+                        <div class="w-full h-auto flex justify-center items-center">
+                            <button class="w-45 h-10 bg-black rounded-xl text-white">Buy Now</button>
+                        </div>
+                    </div>
+                    <div class="max-w-60 h-100 bg-gray-200 rounded-xl flex flex-col gap-3 shadow-xl">
+                        <div class="w-full flex justify-end mb-2 pr-6 pt-3">
+                            <div class="w-7 h-7">
+                                <img :src="heartIcon"/>
+                            </div>
+                        </div>
+                        <div class="w-full h-40 mb-1">
+                            <img :src="iphone" class="w-full h-full object-cover"/>
+                        </div>
+                        <div class="flex flex-col justify-center items-center ">
+                            <p class="text-center">Apple Iphone 14 Pro Max 128GB Deep Pink</p>
+                            <p class="font-bold text-3xl">$900</p>
+                        </div>
+                        <div class="w-full h-auto flex justify-center items-center">
+                            <button class="w-45 h-10 bg-black rounded-xl text-white">Buy Now</button>
+                        </div>
+                    </div>
+                    <div class="max-w-60 h-100 bg-gray-200 rounded-xl flex flex-col gap-3 shadow-xl">
+                        <div class="w-full flex justify-end mb-2 pr-6 pt-3">
+                            <div class="w-7 h-7">
+                                <img :src="heartIcon"/>
+                            </div>
+                        </div>
+                        <div class="w-full h-40 mb-1">
+                            <img :src="iphone" class="w-full h-full object-cover"/>
+                        </div>
+                        <div class="flex flex-col justify-center items-center ">
+                            <p class="text-center">Apple Iphone 14 Pro Max 128GB Deep Pink</p>
+                            <p class="font-bold text-3xl">$900</p>
+                        </div>
+                        <div class="w-full h-auto flex justify-center items-center">
+                            <button class="w-45 h-10 bg-black rounded-xl text-white">Buy Now</button>
+                        </div>
+                    </div>
+                    <div class="max-w-60 h-100 bg-gray-200 rounded-xl flex flex-col gap-3 shadow-xl">
+                        <div class="w-full flex justify-end mb-2 pr-6 pt-3">
+                            <div class="w-7 h-7">
+                                <img :src="heartIcon"/>
+                            </div>
+                        </div>
+                        <div class="w-full h-40 mb-1">
+                            <img :src="iphone" class="w-full h-full object-cover"/>
+                        </div>
+                        <div class="flex flex-col justify-center items-center ">
+                            <p class="text-center">Apple Iphone 14 Pro Max 128GB Deep Pink</p>
+                            <p class="font-bold text-3xl">$900</p>
+                        </div>
+                        <div class="w-full h-auto flex justify-center items-center">
+                            <button class="w-45 h-10 bg-black rounded-xl text-white">Buy Now</button>
+                        </div>
+                    </div>
+                    <div class="max-w-60 h-100 bg-gray-200 rounded-xl flex flex-col gap-3 shadow-xl">
+                        <div class="w-full flex justify-end mb-2 pr-6 pt-3">
+                            <div class="w-7 h-7">
+                                <img :src="heartIcon"/>
+                            </div>
+                        </div>
+                        <div class="w-full h-40 mb-1">
+                            <img :src="iphone" class="w-full h-full object-cover"/>
+                        </div>
+                        <div class="flex flex-col justify-center items-center ">
+                            <p class="text-center">Apple Iphone 14 Pro Max 128GB Deep Pink</p>
+                            <p class="font-bold text-3xl">$900</p>
+                        </div>
+                        <div class="w-full h-auto flex justify-center items-center">
+                            <button class="w-45 h-10 bg-black rounded-xl text-white">Buy Now</button>
+                        </div>
+                    </div>
+                    <div class="max-w-60 h-100 bg-gray-200 rounded-xl flex flex-col gap-3 shadow-xl">
+                        <div class="w-full flex justify-end mb-2 pr-6 pt-3">
+                            <div class="w-7 h-7">
+                                <img :src="heartIcon"/>
+                            </div>
+                        </div>
+                        <div class="w-full h-40 mb-1">
+                            <img :src="iphone" class="w-full h-full object-cover"/>
+                        </div>
+                        <div class="flex flex-col justify-center items-center ">
+                            <p class="text-center">Apple Iphone 14 Pro Max 128GB Deep Pink</p>
+                            <p class="font-bold text-3xl">$900</p>
+                        </div>
+                        <div class="w-full h-auto flex justify-center items-center">
+                            <button class="w-45 h-10 bg-black rounded-xl text-white">Buy Now</button>
+                        </div>
+                    </div>
+                    <div class="max-w-60 h-100 bg-gray-200 rounded-xl flex flex-col gap-3 shadow-xl">
+                        <div class="w-full flex justify-end mb-2 pr-6 pt-3">
+                            <div class="w-7 h-7">
+                                <img :src="heartIcon"/>
+                            </div>
+                        </div>
+                        <div class="w-full h-40 mb-1">
+                            <img :src="iphone" class="w-full h-full object-cover"/>
+                        </div>
+                        <div class="flex flex-col justify-center items-center ">
+                            <p class="text-center">Apple Iphone 14 Pro Max 128GB Deep Pink</p>
+                            <p class="font-bold text-3xl">$900</p>
+                        </div>
+                        <div class="w-full h-auto flex justify-center items-center">
+                            <button class="w-45 h-10 bg-black rounded-xl text-white">Buy Now</button>
+                        </div>
+                    </div>
+                    <div class="max-w-60 h-100 bg-gray-200 rounded-xl flex flex-col gap-3 shadow-xl">
+                        <div class="w-full flex justify-end mb-2 pr-6 pt-3">
+                            <div class="w-7 h-7">
+                                <img :src="heartIcon"/>
+                            </div>
+                        </div>
+                        <div class="w-full h-40 mb-1">
+                            <img :src="iphone" class="w-full h-full object-cover"/>
+                        </div>
+                        <div class="flex flex-col justify-center items-center ">
+                            <p class="text-center">Apple Iphone 14 Pro Max 128GB Deep Pink</p>
+                            <p class="font-bold text-3xl">$900</p>
+                        </div>
+                        <div class="w-full h-auto flex justify-center items-center">
+                            <button class="w-45 h-10 bg-black rounded-xl text-white">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
