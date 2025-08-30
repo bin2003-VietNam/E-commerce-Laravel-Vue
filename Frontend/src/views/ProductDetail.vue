6ad5825f91666595fba6e62cf1ba4872a16a4dee
Nguyen Van Bin <bin2003mail@gmail.com>
2025-08-30 14:07:01 +0700

    chore: add rating section

--- Frontend/src/views/ProductDetail.vue
+++ Frontend/src/views/ProductDetail.vue
@@ -3,7 +3,8 @@ import iphone from "@/assets/images/iphone.jpg"
 import data from "@/data.json"
 import { ref } from "vue";
 import {
-    ChevronDownIcon 
+    ChevronDownIcon,
+    StarIcon
 } from '@heroicons/vue/24/outline'
 
 
@@ -49,8 +50,8 @@ function handle_AddToCart() {
                 <h1 class="text-4xl font-bold">{{ dataProductDetail.title }}</h1>
                 <!-- Price -->
                 <p class="text-3xl font-semibold">
-                    ${{ dataProductDetail.price }}<span
-                        class="text-2xl font-medium ml-4 text-gray-400 line-through">${{ dataProductDetail.discount }}</span>
+                    ${{ dataProductDetail.price }}<span class="text-2xl font-medium ml-4 text-gray-400 line-through">${{
+                        dataProductDetail.discount }}</span>
                 </p>
                 <!-- Color Option -->
                 <div class="flex flex-row justify-start gap-3 items-center">
@@ -97,15 +98,13 @@ function handle_AddToCart() {
                 <!-- Hignlights -->
                 <div class="w-full h-full flex flex-row gap-2">
                     <div class="flex-1/3 flex flex-row gap-3 items-center justify-center"
-                        v-for="highlight in dataProductDetail.highlights"
-                        :key="highlight"
-                    >
+                        v-for="highlight in dataProductDetail.highlights" :key="highlight">
                         <div class="w-[56px] h-[56px] bg-gray-300">
-                            <img :src="highlight.icon" class="object-cover"/>
+                            <img :src="highlight.icon" class="object-cover" />
                         </div>
                         <div class="flex-2/3">
-                            <p class="text-gray-600">{{highlight.title}}</p>
-                            <P class="font-semibold">{{highlight.content}}</P>
+                            <p class="text-gray-600">{{ highlight.title }}</p>
+                            <P class="font-semibold">{{ highlight.content }}</P>
                         </div>
                     </div>
                 </div>
@@ -116,7 +115,12 @@ function handle_AddToCart() {
         <section class="w-full min-h-[896px]  mb-5 p-5">
             <h2 class="text-3xl font-semibold mb-10">Detail</h2>
             <p class="text-lg font-light">
-                Just as a book is judged by its cover, the first thing you notice when you pick up a modern smartphone is the display. Nothing surprising, because advanced technologies allow you to practically level the display frames and cutouts for the front camera and speaker, leaving no room for bold design solutions. And how good that in such realities Apple everything is fine with displays. Both critics and mass consumers always praise the quality of the picture provided by the products of the Californian brand. And last year's 6.7-inch Retina panels, which had ProMotion, caused real admiration for many.
+                Just as a book is judged by its cover, the first thing you notice when you pick up a modern smartphone
+                is the display. Nothing surprising, because advanced technologies allow you to practically level the
+                display frames and cutouts for the front camera and speaker, leaving no room for bold design solutions.
+                And how good that in such realities Apple everything is fine with displays. Both critics and mass
+                consumers always praise the quality of the picture provided by the products of the Californian brand.
+                And last year's 6.7-inch Retina panels, which had ProMotion, caused real admiration for many.
             </p>
             <!-- info 1 -->
             <div>
@@ -133,13 +137,112 @@ function handle_AddToCart() {
             </div>
             <!-- Button  -->
             <div class="flex flex-row w-full h-full justify-center items-center mt-10">
-                <button class="w-auto h-auto px-15 py-3 border-black border-2 rounded-lg font-semibold cursor-pointer">View More <ChevronDownIcon class="w-6 h-6 inline-block"/></button>
+                <button
+                    class="w-auto h-auto px-15 py-3 border-black border-2 rounded-lg font-semibold cursor-pointer">View
+                    More
+                    <ChevronDownIcon class="w-6 h-6 inline-block" />
+                </button>
             </div>
         </section>
-        
-        <!-- Review -->
-        <section class="w-full min-h-[896px] bg-gray-200 mb-5">
 
+        <!-- Review -->
+        <section class="w-full min-h-[896px] flex flex-col justify-start mb-5">
+            <h2 class="mb-10 text-3xl font-semibold">Reviews</h2>
+            <div class="flex flex-row gap-10 ">
+                <div class="max-w-[184px] max-h-[192px] flex-1/5 flex flex-col justify-center items-center bg-gray-100 p-4 gap-3 rounded-3xl">
+                    <p class="text-6xl font-semibold">4.8</p>
+                    <p class="text-xl font-light">of 125 reviews</p>
+                    <div class="flex items-center">
+                        <svg class="ms-1 h-4 w-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 22 20">
+                            <path
+                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg class="w-4 h-4 text-yellow-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 22 20">
+                            <path
+                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg class="w-4 h-4 text-yellow-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 22 20">
+                            <path
+                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg class="w-4 h-4 text-yellow-300 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                            fill="currentColor" viewBox="0 0 22 20">
+                            <path
+                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                        <svg class="w-4 h-4 ms-1 text-gray-300 dark:text-gray-500" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                            <path
+                                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                        </svg>
+                    </div>
+                </div>
+                <div class="flex-4/5 ">
+                    <div class="flex items-center gap-15 w-full">
+                        <!-- Label -->
+                        <span class="text-xl font-medium text-black w-40">Excellent</span>
+
+                        <!-- Progress bar -->
+                        <div class="flex-1 h-2 bg-gray-200 rounded-full overflow-hidden">
+                            <div class="h-full bg-yellow-400 rounded-full" style="width: 80%;"></div>
+                        </div>
+
+                        <!-- Value -->
+                        <span class="text-xl text-gray-500">100</span>
+                    </div>
+                    <div class="flex items-center gap-15 w-full">
+                        <!-- Label -->
+                        <span class="text-xl font-medium text-black w-40">Good</span>
+
+                        <!-- Progress bar -->
+                        <div class="flex-1 h-2 bg-gray-200 rounded-full overflow-hidden">
+                            <div class="h-full bg-yellow-400 rounded-full" style="width: 11%;"></div>
+                        </div>
+
+                        <!-- Value -->
+                        <span class="text-xl text-gray-500">100</span>
+                    </div>
+                    <div class="flex items-center gap-15 w-full">
+                        <!-- Label -->
+                        <span class="text-xl font-medium text-black w-40">Average</span>
+
+                        <!-- Progress bar -->
+                        <div class="flex-1 h-2 bg-gray-200 rounded-full overflow-hidden">
+                            <div class="h-full bg-yellow-400 rounded-full" style="width: 3%;"></div>
+                        </div>
+
+                        <!-- Value -->
+                        <span class="text-xl text-gray-500">100</span>
+                    </div>
+                    <div class="flex items-center gap-15 w-full">
+                        <!-- Label -->
+                        <span class="text-xl font-medium text-black w-40">Below Average</span>
+
+                        <!-- Progress bar -->
+                        <div class="flex-1 h-2 bg-gray-200 rounded-full overflow-hidden">
+                            <div class="h-full bg-yellow-400 rounded-full" style="width: 8%;"></div>
+                        </div>
+
+                        <!-- Value -->
+                        <span class="text-xl text-gray-500">100</span>
+                    </div>
+                    <div class="flex items-center gap-15 w-full">
+                        <!-- Label -->
+                        <span class="text-xl font-medium text-black w-40">poor</span>
+
+                        <!-- Progress bar -->
+                        <div class="flex-1 h-2 bg-gray-200 rounded-full overflow-hidden">
+                            <div class="h-full bg-yellow-400 rounded-full" style="width: 1%;"></div>
+                        </div>
+
+                        <!-- Value -->
+                        <span class="text-xl text-gray-500">100</span>
+                    </div>
+                </div>
+            </div>
         </section>
     </div>
 </template>
